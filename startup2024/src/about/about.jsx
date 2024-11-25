ecdbdc2e308f719b39a9a3c3f5455150f68e6f42
Caleb Drake <cadragon11@gmail.com>
2024-11-25 15:23:11 -0700

    Wrote main.jsx, added necessary files

---
+++ startup2024/src/about/about.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './about.css';
+
+export function About() {
+    return (
+        <main></main>
+    );
+}
---
+++ startup2024/src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
+import { Login } from './login/login'
+import { About } from './about/about'
+import { Account } from './account/account'
+import { Home } from './home/home'
+import { Notebook } from './notebook/notebook'
+
+export default function Main() {
+    return (
+        <BrowserRouter>
+            <header className="header-nav">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div className="logo">GOAL SECRETARY</div>
+                    <menu className='navbar-nav'>
+                        <li className="navbar-item">
+                        <NavLink className='nav-link' to="home">Home</NavLink>
+                        </li>
+                        <li className="navbar-item">
+                            <NavLink className='nav-link' to="notebook">Notebook</NavLink>
+                        </li>
+                        <li className="navbar-item">
+                        <NavLink className='nav-link' to="about">About</NavLink>
+                        </li>
+                        <li className="navbar-item">
+                            <NavLink className='nav-link' to="account">My Account</NavLink>
+                        </li>
+                        <li className="navbar-item">
+                        <NavLink className='nav-link' to="">Login</NavLink>
+                        </li>
+                    </menu>
+                </nav>
+            </header>
+
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/home' element={<Home />} />
+                <Route path='/notebook' element={<Notebook />} />
+                <Route path='/account' element={<Account />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
